a59139f897df6b031078a1c2e3cf0f8791dd6d02
michaelhaneman <mhaneman@calpoly.edu>
2022-10-17 21:24:01 -0700

    updated tractor physics

--- scripts/Bike.cs
+++ scripts/Bike.cs
@@ -3,28 +3,29 @@ using System;
 
 public class Bike : VehicleBody
 {
-    Vector3 jump = new Vector3(-0.1f, 500f, 0.1f);
+    Vector3 jump = new Vector3(-0.1f, 400f, 0.1f);
 
     ScreenControls controls;
+    RayCast rayOnFloor;
     public override void _Ready()
     {
+        rayOnFloor = GetNode<RayCast>("RayCast");
         controls = GetNode<ScreenControls>("ScreenControls");
         controls.Connect("userAction", this, "on_userAction");
+        this.LinearVelocity = new Vector3(0f, 0f, -25f);
     }
 
     public override void _PhysicsProcess(float delta)
     {
-        this.Steering = Input.GetAxis("left", "right") * 0.1f;
+        this.Steering = Input.GetAxis("left", "right") * 0.2f;
         this.EngineForce = -100f;
         
-        if (Input.IsActionJustPressed("jump"))
+        if (Input.IsActionJustPressed("jump") && rayOnFloor.IsColliding())
         {
             this.ApplyCentralImpulse(jump);
         }
 
-        if (this.GlobalTransform.origin.y < -10f || 
-            this.GlobalTransform.origin.x < -10f ||
-            this.GlobalTransform.origin.x > 10f)
+        if (this.GlobalTransform.origin.y < -10f)
         {
             GD.Print("Death Boundary");
         }
